96861e3454dad7d473f97976d1db921cf79de85b
Mr. Aksel Wayne <reakz0169@gmail.com>
2026-07-21 02:09:11 +0800

    Fix MissLog model name

--- app/Models/Misslog.php
+++ app/Models/Misslog.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Misslog extends Model
+class MissLog extends Model
 {
     protected $fillable = [
         'mission_id',
